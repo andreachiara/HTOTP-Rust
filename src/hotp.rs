e9fd45eae6167c2de9d1f40c3f2229e8beceaea7
aki <aki.andreachiara@gmail.com>
2025-08-05 13:34:11 +0200

    It works now. I have no idea honestly, I know how to take a win

--- src/hotp.rs
+++ src/hotp.rs
@@ -7,6 +7,7 @@ pub fn get_totp_from_hmac(hmac_w: [u32; 5], digits: u32) {
     let hmac = to_bytes(hmac_w);
     let offset = usize::from(hmac[19]) & 0xf;
     println!("HMAC is: {} | Offset is {}", to_hex_str(hmac_w), offset);
+    println!("HMAC is: {:?}", hmac);
     let bytes_to_consider = &hmac[offset..offset + 4];
     println!(
         "considering bytes: 0x{:x} | 0x{:x} | 0x{:x} | 0x{:x}",
@@ -20,10 +21,10 @@ pub fn get_totp_from_hmac(hmac_w: [u32; 5], digits: u32) {
     );
     let divider = BASE_TEN.pow(digits);
     let totp = (word & 0x7fffffff) % divider;
-    println!("TOTP: {}", totp);
+    println!("TOTP: {:06}", totp);
 }
 
-fn hex_str_to_bytes_le(s: &str) -> Vec<u8> {
+pub fn hex_str_to_bytes_le(s: &str) -> Vec<u8> {
     let mut ret_vec: Vec<u8> = vec![];
     let mut window_start = s.len();
 
@@ -46,17 +47,18 @@ pub fn hex_str_to_bytes_be(s: &str) -> Vec<u8> {
     let mut ret_vec: Vec<u8> = vec![];
     let mut window_start = 0;
 
+    if window_start % 2 != 0 {
+        println!("stray quartet: 0x{}", &s[window_start..window_start + 1]);
+        ret_vec.push(u8::from_str_radix(&s[window_start..window_start + 1], 16).unwrap());
+        window_start += 1
+    }
+
     loop {
         if window_start + 2 > s.len() {
             break;
         }
         ret_vec.push(u8::from_str_radix(&s[window_start..window_start + 2], 16).unwrap());
         window_start += 2;
-    }
-
-    if window_start != s.len() {
-        println!("stray quartet: 0x{}", &s[window_start..window_start + 1]);
-        ret_vec.push(u8::from_str_radix(&s[window_start..window_start + 1], 16).unwrap());
     }
 
     return ret_vec;
--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,33 @@ mod sha1;
 
 use crate::{hmac::*, hotp::*, sha1::*};
 
+fn generate_hotp_full(key: &[u8], counter: &[u8]) {
+    println!(
+        "Generating HMAC from key: {:?} and counter: {:?}",
+        key, counter
+    );
+    let hmac = hmac_compute(key, counter);
+    //let hmac = hmac_compute(test_key, &counter_bytes[first_nonzero..]);
+    get_totp_from_hmac(hmac, 6);
+}
+
+const BASE32_DIGITS: [u8; 33] = [
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88,
+    89, 90, 50, 51, 52, 53, 54, 55, 61,
+];
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     //let test_string: &str = "hello world this is a long string";
     let test_key = args[1].as_str();
     let test_period: u64 = args[2].parse().unwrap();
-    let test_msg = args[3].as_str();
     let current_time = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs();
     println!("timestamp: {}", current_time);
-    //let counter: u64 = current_time / test_period;
-    let counter: u64 = test_period;
+    let counter: u64 = current_time / test_period;
+    //let counter: u64 = test_period;
     let counter_bytes = counter.to_be_bytes(); // Must be be
 
     let mut last_nonzero: usize = 8;
@@ -50,12 +64,5 @@ fn main() {
         counter, counter, counter_bytes
     );
 
-    let hmac = hmac_compute(
-        &hex_str_to_bytes_be(test_key),
-        &hex_str_to_bytes_be(test_msg),
-    );
-    //let hmac = hmac_compute(test_key, &counter_bytes[first_nonzero..]);
-    get_totp_from_hmac(hmac, 6);
-
-    println!("{}", to_hex_str(hmac));
+    generate_hotp_full(&hex_str_to_bytes_be(test_key), &counter_bytes);
 }
